14240b3267e3ec3e52aa0bdf99a4e817bec8139b
Valor Lin <valor.lin@ringcentral.com>
2023-03-16 13:20:00 +0800

    use github action

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // set baseUrl to ./ to make it work with github pages
+  base: './',
   plugins: [
     react(),
     visualizer({
